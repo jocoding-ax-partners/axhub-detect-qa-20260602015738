b0c175908fa26c7f9aecf3cb6c870b16b2e617f5
realitsyourman <wongil@demodev.io>
2026-06-02 14:55:56 +0900

    fix: make fastapi fixture deployable

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import os
 from fastapi import FastAPI
-
 app = FastAPI()
-
 @app.get('/')
 def root():
-    return {'language': 'python', 'message': os.environ.get('APPHUB_QA_PYTHON_MESSAGE', 'python ok')}
+    return {'ok': 'axhub fastapi'}
