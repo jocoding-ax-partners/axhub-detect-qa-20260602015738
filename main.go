42035aea395ee7ff941142c174aea7fea7418e14
realitsyourman <wongil@demodev.io>
2026-06-02 14:56:13 +0900

    fix: make go fiber fixture deployable

--- main.go
+++ main.go
@@ -1,9 +1,6 @@
 package main
-
-import "github.com/gofiber/fiber/v2"
-
-func main() {
-	app := fiber.New()
-	app.Get("/", func(c *fiber.Ctx) error { return c.JSON(fiber.Map{"ok": true}) })
-	app.Listen(":8080")
-}
+import (
+  "os"
+  "github.com/gofiber/fiber/v2"
+)
+func main(){ port:=os.Getenv("PORT"); if port==""{port="8080"}; app:=fiber.New(); app.Get("/", func(c *fiber.Ctx) error { return c.SendString("axhub go fiber ok")}); app.Listen("0.0.0.0:"+port)}
